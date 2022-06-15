b1d99eb4e4c57dc9d0b3f4a8fcf6c86edc4c1e6e
turehans <96639027+turehans@users.noreply.github.com>
2022-06-15 18:24:59 +1200

    ddfgdfg
    dgdgdg

--- MainInterface.py
+++ MainInterface.py
@@ -1,68 +1,58 @@
 
-def takeInputFromUser(): # Function to take input from user
-    boolean1 = input("Enter your Boolean: ")
-    boolean2 = input("Enter your Second Boolean: ")
-    convention = input("Enter your Convention: ")
-    return boolean1, boolean2, convention
+print("Welcome to the Boolean Calculator!")
+boolean1 = input("Enter your Boolean: ")
+boolean2 = input("Enter your Second Boolean: ")
+convention = input("Enter your Convention: ")
 
-
-
-
- def calculateBooleanResult(boolean1, boolean2, convention):
-    if convention == "AND":
-        if boolean1 == "True" and boolean2 == "True":
-            return "True"
-        else:
-            return "False"
-
-    elif convention == "OR":
-        if boolean1 == "True" or boolean2 == "True":
-            return "True"
-        else:
-            return "False"
-
-    elif convention == "XOR":
-        if boolean1 == "True" and boolean2 == "True":
-            return "False"
-        elif boolean1 == "True" and boolean2 == "False":
-            return "True"
-        elif boolean1 == "False" and boolean2 == "True":
-            return "True"
-        else:
-            return "False"
-
-    elif convention == "NOT":
-        if boolean1 == "True":
-            return "False"
-        else:
-            return "True"
-
-    elif convention == "NAND":
-        if boolean1 == "True" and boolean2 == "True":
-            return "False"
-        else:
-            return "True"
-
-    elif convention == "NOR":
-        if boolean1 == "True" or boolean2 == "True":
-            return "False"
-        else:
-            return "True"
-
-    elif convention == "XNOR":
-        if boolean1 == "True" and boolean2 == "True":
-            return "True"
-        elif boolean1 == "True" and boolean2 == "False":
-            return "False"
-        elif boolean1 == "False" and boolean2 == "True":
-            return "False"
-        else:
-            return "True"
-
+if convention == "AND":
+    if boolean1 == "True" and boolean2 == "True":
+        print("True")
     else:
-        print("Invalid Convention")
-        return "Invalid"
+        print("False")
 
-def run():
-    takeInputFromUser()
-    calculateBooleanResult()
+elif convention == "OR":
+    if boolean1 == "True" or boolean2 == "True":
+        print("True")
+    else:
+        print("False")
+
+elif convention == "XOR":
+    if boolean1 == "True" and boolean2 == "True":
+        print("False")
+    elif boolean1 == "True" and boolean2 == "False":
+        print("True")
+    elif boolean1 == "False" and boolean2 == "True":
+        print("True")
+    else:
+        print("False")
+
+elif convention == "NOT":
+    if boolean1 == "True":
+        print("False")
+    else:
+        print("True")
+
+elif convention == "NAND":
+    if boolean1 == "True" and boolean2 == "True":
+        print("False")
+    else:
+        print("True")
+
+elif convention == "NOR":
+    if boolean1 == "True" or boolean2 == "True":
+        print("False")
+    else:
+        print("True")
+
+elif convention == "XNOR":
+    if boolean1 == "True" and boolean2 == "True":
+        print("True")
+    elif boolean1 == "True" and boolean2 == "False":
+        print("False")
+    elif boolean1 == "False" and boolean2 == "True":
+        print("False")
+    else:
+        print("True")
+
+else:
+    print("Invalid Convention")
